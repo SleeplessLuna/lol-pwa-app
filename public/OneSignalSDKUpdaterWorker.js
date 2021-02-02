acab5bba29a1fca209dbf565ca9457056bba2055
Sleepless Luna <jpcxd1@gmail.com>
2021-02-02 11:17:01 +0100

    initial onesignal setup

---
+++ public/OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,2 @@
+importScripts("http://localhost:5000/service-worker.js");
+importScripts("https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js");
--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import OneSignal from "react-onesignal";
+
+OneSignal.initialize("8da4fc0d-700a-49bb-9d1f-63169284f317");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
