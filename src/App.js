1ab1cb7b53f732d32e441d220facb254e38dfe0b
Sleepless Luna <jpcxd1@gmail.com>
2021-02-04 14:25:06 +0100

    OneSinal SDK testing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 const OneSignal = window.OneSignal || [];
 
 OneSignal.push(function () {
-    /* These examples are all valid */
+    OneSignal.init({
+        appId: "d7e0ffdf-e3cc-418f-b319-8d70e58ccdeb",
+    });
     OneSignal.getUserId(function (userId) {
         console.log("OneSignal User ID:", userId);
         // (Output) OneSignal User ID: 270a35cd-4dda-4b3f-b04e-41d7463a2316
