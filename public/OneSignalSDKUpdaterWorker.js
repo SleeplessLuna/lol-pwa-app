87a274f94ebf2c291bf8a41b87d5839605ad2ac4
Sleepless Luna <jpcxd1@gmail.com>
2021-02-04 09:17:05 +0100

    OneSignal SDK testing

--- public/OneSignalSDKUpdaterWorker.js
+++ public/OneSignalSDKUpdaterWorker.js
@@ -1,2 +1,2 @@
-importScripts("http://localhost:5000/service-worker.js");
+importScripts("https://gallant-bhaskara-82c1bd.netlify.app/service-worker.js");
 importScripts("https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js");
