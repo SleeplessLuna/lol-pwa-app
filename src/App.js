0c7be81d1d8f79988b6ef5f6aacd1efd3dfd9c30
Sleepless Luna <jpcxd1@gmail.com>
2021-02-04 13:26:27 +0100

    OneSignal SDK testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import OneSignal from "react-onesignal";
 
-function App() {
-    let playerID = OneSignal.getPlayerId();
+OneSignal.initialize("d7e0ffdf-e3cc-418f-b319-8d70e58ccdeb");
 
-    console.log(playerID);
+const playerId = await OneSignal.getPlayerId();
+
+console.log(playerId);
+
+function App() {
     return (
         <section>
             <h1>This is a react app</h1>
